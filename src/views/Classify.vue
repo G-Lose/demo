477b71800334817afa7022de577394223171f488
dhpLose <dhpLose@163.com>
2021-09-05 22:48:07 +0800

    声明变量 prams

--- src/views/Classify.vue
+++ src/views/Classify.vue
@@ -1,3 +1,16 @@
 <template>
     <div>分类</div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            msg:"靓仔你好呀"
+        }
+    },
+    created(){
+        let prams = "xiaolan"
+        console.log(prams,"prams")
+    }
+}
+</script>
