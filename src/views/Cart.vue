50a2e623a5164bf541f987792f021ee151772387
G-Lose <G_Lose@163.com>
2021-09-05 23:16:44 +0800

    hit函数

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,3 +1,19 @@
 <template>
     <div>购物车</div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        hit(){
+            let obj = {"姓名":"小花","姓名":"老王"}
+            let objData = Object.assign({},obj,{c:"ccccc"})
+            console.log(objData,'objdatas')
+        }
+    },
+}
+</script>
