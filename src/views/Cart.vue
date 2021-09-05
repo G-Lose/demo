98f48a99ec01a7e677c7571bedc01a6b9c4ae960
dhpLose <dhpLose@163.com>
2021-09-05 22:58:30 +0800

    cart模块

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,3 +1,23 @@
 <template>
     <div>购物车</div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            list:[]
+        }
+    },
+    method:{
+        hit(){
+            let obj = {a:"bbs"}
+            let objData = Object.assign({},obj,{c:"ccccc"})
+            console.log(objData,'objdatas')
+        }
+    },
+    created(){
+        this.hit()
+
+    }
+}
+</script>
